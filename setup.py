82dbb7128249ac278619427a35fef15c0a5e2066
Dan Octavian <danoctavian91@gmail.com>
2019-08-22 12:07:06 +0800

    add basic package files in accordance to https://packaging.python.org/tutorials/packaging-projects/

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qb-sdk",
+    version="0.0.1",
+    author="Qiibee",
+    author_email="tech@qiibee.com",
+    description="A small example package",
+    long_description="SDK for brands to access the qiibee loyalty blockchain.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/qiibee/qb-sdk-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
