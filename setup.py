a7fba5f3971b4d5f4a6d1bdb203377d484d491b0
Dan Octavian <danoctavian91@gmail.com>
2019-10-01 15:09:56 +0800

    add correct dependencies to setup.py and remove requirements.txt

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ setuptools.setup(
     install_requires=[
         'requests>=1.0.0',
         'web3>=5.0.0',
-        'eth-keys>=0.2.0',
-        'eth-utils>=1.6.0'
+        'eth-utils>=1.6.0,<2.0.0',
+        'eth-keys<0.3.0,>=0.2.1',
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
